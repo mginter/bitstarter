65de6165fda630aedfb7235c803baa04ffd1b480
Megan Ginter <mginter@gmail.com>
2013-07-11 04:41:17 +0000

    updated with index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var filename = "index.html";
+var fileBuffer = fs.readFileSync(filename);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
